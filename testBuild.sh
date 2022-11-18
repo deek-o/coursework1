edb86341bc8630b151d72455aecdd53bbc20a8f0
deek-o <declanenglish72@gmail.com>
2022-11-18 05:19:37 +0000

    test

--- testBuild.sh
+++ testBuild.sh
@@ -4,10 +4,16 @@ javac CurrencyConverter.java
 
 bugs=0 #amount of bugs (to increment)
 #CASE 1 a)
-IN1='dollars 1'
+IN1='1 dollars'
 
-EO1="Please enter a valid input"
+EO1="Invalid input. Ending program..."
 
 AO1=$(java CurrencyConverter $IN1)
 
+if $AO1==$EO1
+then
+	echo "test passed"
+else
+	echo "test failed"
+fi
 echo $AO1
