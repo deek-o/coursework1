37e5ab5fb52b229450ec6a0906748860b4d387f0
deek-o <declanenglish72@gmail.com>
2022-11-18 04:30:57 +0000

    test

--- testBuild.sh
+++ testBuild.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+javac CurrencyConverter.java
+
 IN1="dollars 1"
 
 EO1="Please enter a valid input"
 
-AO1=java CurrencyConverter "1 dollars"
+AO1=java CurrencyConverter 1 dollars
 
 echo "pass"
