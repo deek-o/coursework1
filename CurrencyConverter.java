43a43bba2f52fd56cdf829ec0a62e56005bf3ab2
deek-o <declanenglish72@gmail.com>
2022-11-18 12:21:30 +0000

    java fix

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -21,6 +21,8 @@ public class CurrencyConverter {
                 amount = Integer.parseInt(args[0]);
                 String currency = args[1];
 
+		currency.toLowerCase();
+
                 // For amounts Conversion
                 switch (currency){
                         case "dollars" :
